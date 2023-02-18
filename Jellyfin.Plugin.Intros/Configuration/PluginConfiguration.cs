58db8b69d231429bfb646e69a616007ba18155cd
modernharp <106825857+modernharp@users.noreply.github.com>
2023-02-18 18:12:25 +0530

    Add files via upload

---
+++ Jellyfin.Plugin.Intros/Configuration/PluginConfiguration.cs
@@ -0,0 +1,21 @@
+using System;
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.Intros.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public string Local { get; set; } = string.Empty;
+
+        public string Vimeo { get; set; } = string.Empty;
+
+        public int Intro { get; set; } = Plugin.DefaultIntro;
+
+        public int Resolution { get; set; } = Plugin.DefaultResolution;
+
+        public bool Random { get; set; } = false;
+
+        // used internally to track the current intro
+        public Guid Id { get; set; }
+    }
+}
